280c63f07ba78fd4d315accbab05dba1bc9ed7e3
LearningwithMakikoy <marcusmiguel.cuaton@g.msuiit.edu.ph>
2025-10-20 16:17:54 +0800

    front-end dev (error occurred will need to fix)

--- run.py
+++ run.py
@@ -1,7 +1,22 @@
-from app import create_app
-import os
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
-if __name__ == "__main__":
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/students')
+def students():
+    return render_template('students.html')
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/admin/create')
+def admin_create():
+    return render_template('admin_create.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
