80965d6a607772b99320020e800fb92747cd35bc
LearningwithMakikoy <marcusmiguel.cuaton@g.msuiit.edu.ph>
2025-10-20 16:26:49 +0800

    fixed bug for the webapp not running on local.host

--- run.py
+++ run.py
@@ -4,19 +4,19 @@ app = Flask(__name__, template_folder='app/templates', static_folder='app/static
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('layouts/index.html')
 
 @app.route('/students')
 def students():
-    return render_template('students.html')
+    return render_template('layouts/students.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
-    return render_template('login.html')
+    return render_template('layouts/login.html')
 
 @app.route('/admin/create')
 def admin_create():
-    return render_template('admin_create.html')
+    return render_template('layouts/admin_create.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
